135988e52908b4dce20881b2758c395d332bd842
rshafiur127@gmail.com <rshafiur127@gmail.com>
2024-02-24 03:31:48 +0000

    commit

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -13,13 +13,27 @@ with tripdata as
   where dispatching_base_num is not null 
   and SUBSTRING('pickup_datetime', 1, 4) = '2019'
 )
-select *
+select
+    -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,
+    
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- casting
+	cast(dispatching_base_num as string) as dispatching_base_num,
+	cast(Affiliated_base_number as string) as affiliated_base_number,
+	cast(SR_Flag as string) as sr_falg
+
 from tripdata
 where rn = 1
 
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var('is_test_run', default=true) %}
+{% if var('is_test_run', default=false) %}
 
   limit 100
 
