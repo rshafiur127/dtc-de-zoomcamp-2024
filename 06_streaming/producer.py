0d40c9c13ca5f68ed6b74a7e0d1a5948d084d0f8
rshafiur127 <rshafiur127@gmail.com>
2024-03-19 12:31:35 +0600

    Create producer.py

---
+++ 06_streaming/producer.py
@@ -0,0 +1,52 @@
+import os
+import pathlib
+import time
+import csv
+import json
+from typing import List, Dict
+from kafka import KafkaProducer
+from kafka.errors import KafkaTimeoutError
+from kafka.errors import KafkaError
+
+from ride import Ride
+from settings import BOOTSTRAP_SERVERS, INPUT_DATA_PATH, KAFKA_TOPIC
+
+
+class JsonProducer(KafkaProducer):
+    def __init__(self, props: Dict):
+        self.producer = KafkaProducer(**props)
+
+    @staticmethod
+    def read_records(resource_path: str):
+        records = []
+        with open(resource_path, 'r') as f:
+            reader = csv.reader(f)
+            header = next(reader)  # skip the header row
+            for row in reader:
+                records.append(Ride(arr=row))
+        return records
+
+    def publish_rides(self, topic: str, messages: List[Ride]):
+        for ride in messages:
+            try:
+                record = self.producer.send(topic=topic, key=ride.VendorID, value=ride)
+                print('Record {} successfully produced at offset {}'.format(ride.VendorID, record.get().offset))
+            except KafkaTimeoutError as e:
+                print(e.__str__())
+
+
+if __name__ == '__main__':
+    # Config Should match with the KafkaProducer expectation
+    # kafka expects binary format for the key-value pair
+    here = pathlib.Path(__file__).parent.resolve()
+    t0 = time.time()
+    config = {
+        'bootstrap_servers': BOOTSTRAP_SERVERS,
+        'key_serializer': lambda key: str(key).encode(),
+        'value_serializer': lambda x: json.dumps(x.__dict__, default=str).encode('utf-8')
+    }
+    producer = JsonProducer(props=config)
+    rides = producer.read_records(resource_path=INPUT_DATA_PATH)
+    producer.publish_rides(topic=KAFKA_TOPIC, messages=rides)
+    t1 = time.time()
+    print(f'took {(t1 - t0):.2f} seconds')
